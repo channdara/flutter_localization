08d156731de253125f41e677aeb116e29de24790
e.chandara <eamchandara@labs.haystack.asia>
2023-12-11 21:02:13 +0700

    [UNTRACKED]: add scriptCode to MapLocale

--- lib/src/utility/preference_util.dart
+++ lib/src/utility/preference_util.dart
@@ -7,6 +7,7 @@ mixin PreferenceUtil {
   static const String _locale_key = 'translator_locale_key';
   static const String _language_code = 'translator_language_code';
   static const String _country_code = 'translator_country_code';
+  static const String _script_code = 'translator_script_code';
 
   /// Load the saved locale in the shared_preferences.
   static Future<Locale?> _getLocale() async {
@@ -14,9 +15,10 @@ mixin PreferenceUtil {
     final prefData = pref.getString(_locale_key);
     if (prefData == null) return null;
     final locale = jsonDecode(prefData) as Map<String, dynamic>;
-    return Locale(
-      locale[_language_code].toString(),
-      locale[_country_code].toString(),
+    return Locale.fromSubtags(
+      languageCode: locale[_language_code].toString(),
+      countryCode: locale[_country_code] as String?,
+      scriptCode: locale[_script_code] as String?,
     );
   }
 
@@ -24,18 +26,29 @@ mixin PreferenceUtil {
   static Future<bool> setLocale(
     String languageCode,
     String? countryCode,
+    String? scriptCode,
   ) async {
     final pref = await SharedPreferences.getInstance();
     final value = json.encode({
       _language_code: languageCode,
       _country_code: countryCode,
+      _script_code: scriptCode,
     });
     return pref.setString(_locale_key, value);
   }
 
   /// Generate the current locale which will use for displaying the language
   /// which the app is change during translate() function called.
-  static Future<Locale?> getInitLocale(String? lInit, String? cInit) async {
-    return await _getLocale() ?? Locale(lInit ?? 'en', cInit ?? '');
+  static Future<Locale?> getInitLocale(
+    String? languageCode,
+    String? countryCode,
+    String? scriptCode,
+  ) async {
+    return await _getLocale() ??
+        Locale.fromSubtags(
+          languageCode: languageCode ?? 'en',
+          countryCode: countryCode,
+          scriptCode: scriptCode,
+        );
   }
 }
